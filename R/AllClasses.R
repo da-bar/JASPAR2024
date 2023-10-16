387ac013286d599c94258a5e30c9f61864494588
Damir <dbaranasic1@gmail.com>
2023-10-16 09:27:54 +0100

    Correct ERROR, WARNING, and NOTE from the BiocBuilder

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -9,11 +9,34 @@
 #' @keywords classes
 #' @examples
 #'
-#' JASPAR2024@db
+#' library(JASPAR2024)
+#' JASPAR2024
 #'
 #' @import methods
 #' @exportClass JASPAR2024
 setClass("JASPAR2024", slots = c(db = "character")
          )
 
+#' @name db
+#'
+#' @title Access database from JASPAR2024 object
+#' @description The accessor function for retrieving the location of the
+#' database location slot from the JASPAR2024 object
+#' @author Damir Baranasic
+#' @keywords function
+#' @examples
+#'
+#' library(JASPAR2024)
+#' db(JASPAR2024)
+#'
+#' @import methods
+#' @export
 
+setGeneric("db", function(object){
+  standardGeneric("db")
+})
+
+setMethod("db", "JASPAR2024",
+          function(object){
+            object@db
+          })
